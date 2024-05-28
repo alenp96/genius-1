d030713339ce50fdc286975e5ea780641458e467
kelvinrandu <randukelvin@gmail.com>
2024-05-28 13:55:56 +0300

    comment sub scription

--- theworkerbulls-saas/app/api/subscription/route.ts
+++ theworkerbulls-saas/app/api/subscription/route.ts
@@ -14,14 +14,14 @@ export async function GET() {
     const user = await currentUser();
     console.log('user-->',user?.emailAddresses[0].emailAddress)
     
-    // const sub = await hasSubscription();
+    const sub1 = await hasSubscription();
     // const customer = await createCustomerIfNull(String(user?.emailAddresses[0]?.emailAddress));
     // const checkoutLink = await createCheckoutLink(String(customer));
     // console.log('subs',sub,customer,checkoutLink)
     const sub = false;
     // const customer = await createCustomerIfNull(String(user?.emailAddresses[0]?.emailAddress));
     const checkoutLink = '/dashboard'
-    console.log('subs',sub,checkoutLink)
+    console.log('subs',sub1,checkoutLink)
     // if (!userId || !user) {
     //   return new NextResponse("Unauthorized", { status: 401 });
     // }
