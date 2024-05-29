9b3a1076c5adef2c76db7cc3585841e078edc203
kelvinrandu <randukelvin@gmail.com>
2024-05-29 14:57:39 +0300

    added success url

--- theworkerbulls-saas/lib/stripe.ts
+++ theworkerbulls-saas/lib/stripe.ts
@@ -34,7 +34,7 @@ export async function hasSubscription() {
 }
 export async function createCheckoutLink(customer: string) {
   const checkout = await stripe.checkout.sessions.create({
-    success_url: "https://genius-beta-lac.vercel.app/profile/",
+    success_url: "https://genius-beta-lac.vercel.app/sub/",
     cancel_url: "https://genius-beta-lac.vercel.app/profile",
     customer: customer,
     payment_method_types: ["card"],
