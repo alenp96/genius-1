d567991baeb84dca6ae2eb29392ed19fb942da39
kelvinrandu <randukelvin@gmail.com>
2024-05-29 16:36:14 +0300

    api fix

--- theworkerbulls-saas/app/api/subscription1/route.ts
+++ theworkerbulls-saas/app/api/subscription1/route.ts
@@ -4,18 +4,23 @@ import { NextResponse } from "next/server";
 import prismadb from "@/lib/prismadb";
 import { stripe } from "@/lib/stripe";
 import { absoluteUrl } from "@/lib/utils";
-import { NextApiRequest, NextApiResponse } from "next";
 
 
 const settingsUrl = absoluteUrl("/settings");
 
-export default async (request: NextApiRequest, response: NextApiResponse) => {
-  const {
-    query: { name, keyword },
-    method,
-  } = request;
-  console.log(name, keyword, method);
+export async function GET(req:any) {
+  console.log('request',req)
+  try {
+    const { userId,user } = auth();
+ 
 
-  // do nothing fancy and simply return a string concatenation
-  return response.status(200).json({ query: name + " " + keyword });
+
+
+    return new NextResponse(JSON.stringify({ state: 'sessionid back'}))
+
+    // return new NextResponse(JSON.stringify({ url: 'url' }))
+  } catch (error) {
+    console.log("[STRIPE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 };
