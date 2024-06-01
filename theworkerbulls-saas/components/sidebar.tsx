617a0b2938a72abc3d4e92f2824902b6e056b9b3
kelvinrandu <randukelvin@gmail.com>
2024-06-01 16:28:01 +0300

    added spiner and big logo sizes

--- theworkerbulls-saas/components/sidebar.tsx
+++ theworkerbulls-saas/components/sidebar.tsx
@@ -19,7 +19,7 @@ import {
   Bot,
 } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { Spinner } from '@chakra-ui/react'
+import { Spinner,Center } from '@chakra-ui/react'
 import { cn } from "@/lib/utils";
 
 const poppins = Montserrat({ weight: "600", subsets: ["latin"] });
@@ -91,14 +91,22 @@ export const Sidebar = ({
 
   }, [])
   if (loaded) {
-    return <Spinner />;
+    return (<> <Center>
+            <Spinner
+    thickness='4px'
+    speed='0.65s'
+    emptyColor='gray.200'
+    color='white.500'
+    size='xl'
+  />;
+      </Center></>);
 }
   return (
     <div className="space-y-4  py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
-          <div className="relative h-8 w-8 mr-4">
-            <Image fill alt="Logo" src="/logo.png" />
+          <div className="relative h-16 w-40 mr-4">
+            <Image fill alt="Logo" src="/logo.jpeg" />
           </div>
           {/* <h1 className={cn("text-2xl font-bold", poppins.className)}>
             Genius
