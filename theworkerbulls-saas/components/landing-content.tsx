62dfd7bf8723bf21b3a521c7309348cc7f611f06
kelvinrandu <randukelvin@gmail.com>
2024-06-12 14:00:32 +0300

    added pricing styles

--- theworkerbulls-saas/components/landing-content.tsx
+++ theworkerbulls-saas/components/landing-content.tsx
@@ -276,47 +276,68 @@ export default function LandingContent() {
           </VStack>
         </PriceWrapper>
 
+ 
         <PriceWrapper>
-          <Box py={4} px={12}>
-            <Text  color={'white'} fontWeight="500" fontSize="2xl">
-            Monthly
-            </Text>
-            <HStack justifyContent="center">
-              <Text  color={'white'} fontSize="3xl" fontWeight="600">
-                $
+          <Box position="relative">
+            <Box
+              position="absolute"
+              top="-16px"
+              left="50%"
+              style={{ transform: 'translate(-50%)' }}>
+              <Text
+                textTransform="uppercase"
+                bg={useColorModeValue('red.300', 'red.700')}
+                px={3}
+                py={1}
+                color={useColorModeValue('gray.900', 'gray.300')}
+                fontSize="sm"
+                fontWeight="600"
+                rounded="xl">
+                (25% off)
               </Text>
-              <Text   color={'white'}fontSize="5xl" fontWeight="900">
-                15
-              </Text>
-              <Text fontSize="3xl" color="gray.500">
-                /month   (25% off)
-              </Text>
-            </HStack>
-          </Box>
-          <VStack
-            // bg={useColorModeValue('gray.50', 'gray.700')}
-            py={4}
-            borderBottomRadius={'xl'}>
-            <List spacing={3} textAlign="start" px={12}>
-              <ListItem  color={'white'}>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                24/7 Availability	
-              </ListItem>
-              <ListItem  color={'white'}>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                Personalized Advice
-              </ListItem>
-              <ListItem  color={'white'}>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                Expert-Backed Tips
-              </ListItem>
-            </List>
-            <Box w="80%" pt={7}>
-              <Button w="full" colorScheme="red" variant="outline">
-                Start trial
-              </Button>
             </Box>
-          </VStack>
+            <Box py={4} px={12}>
+              <Text fontWeight="500" fontSize="2xl">
+              Monthly
+              </Text>
+              <HStack justifyContent="center">
+                <Text fontSize="3xl" fontWeight="600">
+                  $
+                </Text>
+                <Text fontSize="5xl" fontWeight="900">
+                  15
+                </Text>
+                <Text fontSize="3xl" color="gray.500">
+                  /month
+                </Text>
+              </HStack>
+            </Box>
+            <VStack
+      
+              py={4}
+              borderBottomRadius={'xl'}>
+              <List spacing={3} textAlign="start" px={12}>
+                <ListItem color={'white'}> 
+                  <ListIcon as={FaCheckCircle} color="green.500" />
+                  24/7 Availability
+                </ListItem>
+                <ListItem color={'white'}>
+                  <ListIcon as={FaCheckCircle} color="green.500" />
+                  Personalized Advice
+                </ListItem>
+                <ListItem color={'white'}>
+                  <ListIcon as={FaCheckCircle} color="green.500" />
+                   Expert-Backed Tips
+                </ListItem>
+        
+              </List>
+              <Box w="80%" pt={7}>
+                <Button w="full" colorScheme="red">
+                  Start trial
+                </Button>
+              </Box>
+            </VStack>
+          </Box>
         </PriceWrapper>
       </Stack>
     </Box>
